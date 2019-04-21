234ce7dd1b2298fdb79e1d76148e3198ba9372b9
DocMcCoy30 <thomas.pretet@outlook.fr>
2019-04-21 20:08:39 +0200

    creation jar

---
+++ src/EscapeGame.java
@@ -0,0 +1,10 @@
+public class EscapeGame {
+
+    //Configuration config;
+
+    public static void main(String[] args){
+        Menu run = new Menu();
+        //if (args != null) config.getModeDev = true;
+        run.modeChoix();
+    }
+}
